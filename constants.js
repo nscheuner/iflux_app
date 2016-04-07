05dbc6d370a094733c2199c8a5330e224b5bdbe3
kretinus <kretinus@bluewin.ch>
2016-04-07 13:54:57 +0200

    constant

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 angular.module('iflux.constants', [])
   .constant('apiUrl', '@apiUrl@')
-  .constant('quimgUrl', "@qimgUrl@")
-  .constant('quimgToken', "@qimgToken@")
+  .constant('qimgUrl', "@qimgUrl@")
+  .constant('qimgToken', "@qimgToken@")
   .constant('mapboxMapId', "kretinus.cifqotirh025ctnly00jb6edq")
   .constant('mapboxAccessToken', "pk.eyJ1Ijoia3JldGludXMiLCJhIjoiY2lmcW90ajFpMDI1Z3RjbHhzMm5jcXBkeiJ9.L0oWG_Iv_q4TPid_BLmFog")
 ;
